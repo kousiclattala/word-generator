8a15dd3a93246bde26726c8792df3f3470f9d00b
kousiclattala <kousiclattala112@gmail.com>
2021-01-11 18:46:18 +0530

    Word Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import arrayWords from '../../utils/words';
+import countryNames from '../../utils/countries';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  countries = '';
+  limit = 10;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
+
+  generateCountries() {
+    this.countries = countryNames.slice(0, this.limit).join(' ');
+  }
 }
